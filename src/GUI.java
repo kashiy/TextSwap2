78b96040a86d57b63629218648b5a488333e7897
Yuval Kashi <kashiy@post.bgu.ac.il>
2021-04-16 22:18:09 +0300

    1.splitted the getHebFromClip to a get and set 2.added JTextarea for copied and pasted and updated the actionlistener

--- src/GUI.java
+++ src/GUI.java
@@ -6,21 +6,41 @@ import java.awt.event.ActionListener;
 //todo this should be a singleton
 public class GUI implements ActionListener {
 
+    private JFrame frame;
+    private JButton button;
+    private JLabel label;
+    private JTextArea copiedTextArea, pastedTextArea;
+    private JPanel panel;
     private TextSwap swapper;
+    private String copiedFromBoard="";
+    private String pastedToBoard="";
+
 
     public GUI(){
-        JFrame frame = new JFrame();
 
-        JButton button = new JButton("Paste");
+        frame = new JFrame();
+        button = new JButton("Paste");
         swapper = new TextSwap();
         button.addActionListener(this);
-        JLabel label = new JLabel("Swapped to Heb");
+        label = new JLabel("Swapped to Heb");
 
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(100,100,30,100));
+        //todo - check why width is stuck
+        copiedTextArea = new JTextArea(copiedFromBoard);
+        copiedTextArea.setBounds(250,250,250,200);
+        copiedTextArea.setEditable(false);
+        pastedTextArea = new JTextArea(copiedFromBoard);
+        pastedTextArea.setBounds(20,75,250,200);
+        pastedTextArea.setEditable(false);
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30,250,50,250));
         panel.setLayout(new GridLayout(0,1));
+
+        panel.add(copiedTextArea);
         panel.add(button);
         panel.add(label);
+        panel.add(pastedTextArea);
+
 
         frame.add(panel,BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,6 +51,11 @@ public class GUI implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        swapper.getHebFromClip();
+        copiedFromBoard = swapper.getHebFromClip();
+        copiedTextArea.setText(copiedFromBoard);
+        pastedToBoard = swapper.setHebFromClip(copiedFromBoard);
+        pastedTextArea.setText(pastedToBoard);
     }
 }
+
+//todo - add an optoin for text be shown with keyboard shorcuts combinations.
--- src/TextSwap.java
+++ src/TextSwap.java
@@ -9,53 +9,66 @@ public class TextSwap {
 
     public static void main(String[] args) {
         GUI a = new GUI();
-//
-//        Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
-//        int i=1;
-//        while(i != 0) {
-//            Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
-//            try {
-//                if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-//                    String text = (String)t.getTransferData(DataFlavor.stringFlavor);
-//                    System.out.println(text);
-//                    String newtext = Swap.toHeb(text);
-//                    System.out.println(newtext);
-//                    //paste to clipboard
-//                    StringSelection ss = new StringSelection(newtext);
-//                    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
-//                }
-//            } catch (Exception ex) {
-//                ex.printStackTrace();
-//            }
-//
-//            Scanner myScanner= new Scanner(System.in);
-//            i= myScanner.nextInt();
-
     }
 
+
     public String getHebFromClip(){
-        String newText= "";
+        String text= "";
         Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
         Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
         try {
             if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-                String text = (String)t.getTransferData(DataFlavor.stringFlavor);
-                System.out.println(text);
-                newText = Swap.toHeb(text);
-                System.out.println(newText);
-                //paste to clipboard
-                StringSelection ss = new StringSelection(newText);
-                Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
-                
+                text = (String)t.getTransferData(DataFlavor.stringFlavor);
+                System.out.println("copied: " + text);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+        return text;
+    }
+
+    public String setHebFromClip(String hebText){
+        String newText= "";
+        try {
+                newText = Swap.toHeb(hebText);
+                System.out.println("pasted: " + newText);
+                //paste to clipboard
+                StringSelection ss = new StringSelection(newText);
+                Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+            }
+         catch (Exception ex) {
+            ex.printStackTrace();
+        }
         return newText;
     }
+
 }
 
 
-//                    tct
-//                    nv eurv ht nkl vmkj, kf,uc ,ufbh, aknv annhrv?
+
+//nv eurv ht nkl vmkj, kf,uc ,ufbh, aknv annhrv?
 //fh; kepu. kcrhfv farumho kgu; k,ul vnho vgnueho
+
+
+/*
+        Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
+        int i=1;
+        while(i != 0) {
+            Transferable t = Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+            try {
+                if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    String text = (String)t.getTransferData(DataFlavor.stringFlavor);
+                    System.out.println(text);
+                    String newtext = Swap.toHeb(text);
+                    System.out.println(newtext);
+                    //paste to clipboard
+                    StringSelection ss = new StringSelection(newtext);
+                    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            Scanner myScanner= new Scanner(System.in);
+            i= myScanner.nextInt();
+*/
