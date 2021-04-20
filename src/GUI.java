baad31d68222c71bdea554142577d1fa3b16cbbf
Yuval Kashi <kashiy@post.bgu.ac.il>
2021-04-20 15:19:33 +0300

    //todo- the robot is a problem because i am not in the same program and this is not working underground

--- src/GUI.java
+++ src/GUI.java
@@ -9,7 +9,8 @@ import java.awt.event.KeyListener;
 public class GUI implements ActionListener {
 
     private JFrame frame;
-    private JButton button;
+    private JButton swap_Button;
+    private JButton swapAndPaste_Button;
     private JLabel label;
     private JTextArea copiedTextArea, pastedTextArea;
     private JPanel panel;
@@ -29,11 +30,20 @@ public class GUI implements ActionListener {
     private GUI(){
 
         frame = new JFrame();
-        button = new JButton("Paste");
+
         swapper = new TextSwap();
-        button.addActionListener(this);
-        //alt+c add an optoin for text be shown with keyboard shorcuts combinations.
-        button.setMnemonic('c');
+
+        swap_Button = new JButton("swap on clipboard");
+        swap_Button.addActionListener(this);
+        //alt+c will act like the button and paste
+        swap_Button.setMnemonic('c');
+
+        swapAndPaste_Button = new JButton("swap and paste on spot");
+        SwapAndPaste listener = new SwapAndPaste();
+        swapAndPaste_Button.addActionListener(listener);
+        //alt+v will act like the button and paste
+        swapAndPaste_Button.setMnemonic('v');
+
         label = new JLabel("Swapped to Heb");
 
         //todo - check why width is stuck
@@ -49,7 +59,8 @@ public class GUI implements ActionListener {
         panel.setLayout(new GridLayout(0,1));
 
         panel.add(copiedTextArea);
-        panel.add(button);
+        panel.add(swap_Button);
+        panel.add(swapAndPaste_Button);
         panel.add(label);
         panel.add(pastedTextArea);
 
@@ -63,10 +74,42 @@ public class GUI implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+        System.out.println("swap");
         copiedFromBoard = swapper.getHebFromClip();
         copiedTextArea.setText(copiedFromBoard);
         pastedToBoard = swapper.setHebFromClip(copiedFromBoard);
         pastedTextArea.setText(pastedToBoard);
+    }
+
+    //new ActionListener for the swapAndPaste_Button
+    public class SwapAndPaste implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("SwapAndPaste");
+            try
+            {
+                //todo- the robot is a problem because i am not in the same program and this is not working underground
+//                Robot r = new Robot();
+//                r.keyPress(KeyEvent.VK_CONTROL);
+//                r.keyPress(KeyEvent.VK_C);
+//                r.keyRelease(KeyEvent.VK_CONTROL);
+//                r.keyRelease(KeyEvent.VK_C);
+                copiedFromBoard = swapper.getHebFromClip();
+                copiedTextArea.setText(copiedFromBoard);
+                pastedToBoard = swapper.setHebFromClip(copiedFromBoard);
+                pastedTextArea.setText(pastedToBoard);
+//                r.keyPress(KeyEvent.VK_CONTROL);
+//                r.keyPress(KeyEvent.VK_V);
+//                r.keyRelease(KeyEvent.VK_CONTROL);
+//                r.keyRelease(KeyEvent.VK_V);
+
+            }
+            catch(Exception ex)
+            {
+                System.out.println(ex);
+            }
+        }
     }
 /*
     @Override
